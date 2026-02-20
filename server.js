0f56c00a1b69b25ffbbe9d50ae73b29a457c610e
Naveen Yadav <126949739+Naveen-Yaduvanshi@users.noreply.github.com>
2026-02-20 15:14:02 +0530

    Reorder firebase-admin import after dotenv

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const admin = require("firebase-admin");
+
 
 require("dotenv").config();
 const admin = require("firebase-admin");
