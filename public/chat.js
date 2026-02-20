3b10b4e5cb9f0cb1af3b8336ead31a3a64826d8a
Naveen Yadav <126949739+Naveen-Yaduvanshi@users.noreply.github.com>
2026-02-20 15:45:30 +0530

    Update socket connection URL in chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-const socket = io();
+const socket = io(saathispace-production.up.railway.app);
 
 const chatBox = document.getElementById("chatBox");
 const input = document.getElementById("messageInput");
